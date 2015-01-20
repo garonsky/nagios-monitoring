a11816b9aa94abb8198a888e676200141114152e
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2015-01-20 16:23:18 -0500

    HPCC-12716 Nagios Monitoring for HPCC 5.2
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -1,9 +1,10 @@
 ###
 ## Version Information
 ###
-set ( HPCC_NM_MAJOR 4 )
-set ( HPCC_NM_MINOR 3 )
-set ( HPCC_NM_POINT 0 )
-set ( HPCC_NM_MATURITY "trunk" )
-set ( HPCC_NM_SEQUENCE 1 )
+set ( HPCC_PROJECT "community" )
+set ( HPCC_MAJOR 5 )
+set ( HPCC_MINOR 2 )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "closedown" )
+set ( HPCC_SEQUENCE 0 )
 ###
